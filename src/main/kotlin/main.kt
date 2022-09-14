26d527fb1c9b47ef3ae6d56d25887ff69d2b8fac
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-09-14 17:23:27 -0400

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var fullName = "Dmitriy Malayev"
+    val emptyName = ""
+    println("Hello World")
+    println(fullName)
+    print(emptyName.isEmpty())
+    fullName = "ok"
+    println(fullName)
+
+//    val is read only, var is not
+}
