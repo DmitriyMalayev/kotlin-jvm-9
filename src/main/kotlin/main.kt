b7d4df275aa0d6b617b3559eab0e2eef91abfc18
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-09-14 23:41:40 -0400

    updates

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,31 @@
 fun main(){
     var fullName = "Dmitriy Malayev"
+    val lastName: String = "Malayev"   //explicitly providing data type to a variable
     val emptyName = ""
     println("Hello World")
     println(fullName)
     print(emptyName.isEmpty())
-    fullName = "ok"
+    fullName = "okie"
     println(fullName)
+    println(fullName::class)
+    println(lastName)
 
-//    val is read only, var is not
+    val myByte: Byte = 8  //8-bit signed integer
+    val myShort: Short = 16 //16-bit signed integer
+    val myInt : Int = 32  // 32-bit signed integer   (most common)
+    val myLong : Long = 64 //64-bit signed integer
+
+    //Decimals
+    val myFloat: Float = 32.00F //32-bit floating point number
+    val myDouble: Double = 64.00 //64-bit floating point number
+
+//    println(myByte.toDouble())
+
+
+
+
+/*
+val is read only
+var can be reassigned
+*/
 }
